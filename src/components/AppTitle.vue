2709ca9172d4a505198ff87a1e62e48ee3cee31f
Oleksandra Ubska <alessandraubska@gmail.com>
2024-07-04 17:09:21 +0200

    add components AppTitle.vue

---
+++ src/components/AppTitle.vue
@@ -0,0 +1,14 @@
+<script>
+// qui va il codice javascript
+export default {
+    name: "AppTitle",
+}
+</script>
+
+<template>
+   <h1>La mia prima app con Vite!</h1>
+</template>
+
+<style scoped>
+/* qui aggiungiamo css */
+</style>
